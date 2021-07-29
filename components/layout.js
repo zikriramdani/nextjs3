91907bf309d76542b8eb958c323589071945fc6e
ZR <zikriramdani.developer@gmail.com>
2021-07-29 12:53:51 +0700

    M

--- components/layout.js
+++ components/layout.js
@@ -1,11 +1,5 @@
 import styles from '../styles/Home.module.css'
 
 export default function Layout({ children }) {
-    return <div className={styles.container}>
-        <main className={styles.main}>
-            <div className={styles.grid}>
-                {children}
-            </div>
-        </main>
-    </div>
+    return <div className={styles.container}>{children}</div>
 }
--- pages/pagetwo/index.js
+++ pages/pagetwo/index.js
@@ -1,18 +1,28 @@
+import Head from 'next/head'
 import Layout from '../../components/layout';
 import Link from "next/link";
+import styles from '../../styles/Home.module.css'
 
 export default function Pagetwo() {
     return (
         <Layout>
-            <h1>Page 2</h1>
-            <p>
-                Lorem Ipsum is simply dummy text of the printing and typesettingindustry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged
-            </p>
-            <Link href="/">
-                <a>
-                    <p>Back to Home</p>
-                </a>
-            </Link>
+            <Head>
+                <title>Page 2 - Create Next App</title>
+            </Head>
+
+            <main className={styles.main}>
+                <div className={styles.grid}>
+                    <h1>Page 2</h1>
+                    <p>
+                        Lorem Ipsum is simply dummy text of the printing and typesettingindustry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged
+                    </p>
+                    <Link href="/">
+                        <a>
+                            <p>Back to Home</p>
+                        </a>
+                    </Link>
+                </div>
+            </main>
         </Layout>
     );
 }
