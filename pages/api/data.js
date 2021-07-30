7afb0a35f02cdc02baa3d478b7cd3ee7793d77ff
ZR <zikriramdani.developer@gmail.com>
2021-07-31 05:57:08 +0700

    M

--- pages/api/data.js
+++ pages/api/data.js
@@ -5,12 +5,11 @@ export default async (req, res) => {
 
     if (session) {
         res.status(200).json({
-        message: 'You can access this content because you are signed in.',
+            message: 'You can access this content because you are signed in.',
         })
     } else {
         res.status(403).json({
-        message:
-            'You must be sign in to view the protected content on this page.',
+            message: 'You must be sign in to view the protected content on this page.',
         })
     }
 }
