9edc5bf75a7004db9d43571fb88f7dd2f524e998
ZR <zikriramdani.developer@gmail.com>
2021-07-31 09:11:35 +0700

    M

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -1,10 +1,18 @@
 import Head from 'next/head'
 import Layout from '../../components/layout';
 import Link from "next/link";
-import styles from '../../styles/Home.module.css'
+import styles from '../../styles/Home.module.css';
+import Footer from '../../components/footer';
+import { useRouter } from 'next/router';
 // import { getSession } from 'next-auth/client'
 
-export default function Dashboard({ user }) {
+export default function Dashboard({ user, props }) {
+    const router = useRouter();
+    // handle click event of logout button
+    const handleLogout = () => {    
+        router.push('/');
+    }
+
     return (
         <Layout>
             <Head>
@@ -17,13 +25,14 @@ export default function Dashboard({ user }) {
                     {/* <p>Welcome {user.email}</p> */}
                 </div>
                 <div className={styles.grid}>
-                    <Link href="/">
-                        <a>
-                            <p>Logout</p>
-                        </a>
-                    </Link>
+                    {/* <input type="button" onClick={handleLogout} value="Logout" /> */}
+                    <a onClick={handleLogout} className={styles.cursorPointer}>
+                        <p>Logout</p>
+                    </a>
                 </div>
             </main>
+
+            <Footer></Footer>
         </Layout>
     )
 }
