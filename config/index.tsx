83d8842c9c63b4e2e46db630a617a34b1514831c
ZR <zikriramdani.developer@gmail.com>
2021-08-05 09:26:36 +0700

    M

--- config/index.tsx
+++ config/index.tsx
@@ -2,9 +2,5 @@ export const webConfig = {
 	baseUrl:{
 		baseUrlProd: 'https://reqres.in/api/',
 		baseUrlChart: 'https://gold-price-live.p.rapidapi.com/'
-	},
-	// headerParams:{
-	// 	partnercode: 24508,
-	// 	apikey:'HTBcrQU49Qf7syks5EUuRCFGv3q2Xkfa9'
-	// }
+	}
 }
